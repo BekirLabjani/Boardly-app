44f27004482d40749adb9e63da62eea789df8366
BekirLajani <business.life.bekir@gmail.com>
2024-11-14 16:47:48 +0100

    add Task

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { Task } from '../models/task'; 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent,HeaderComponent],
+  imports: [HeaderComponent,SidebarComponent,HeaderComponent ,FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+  @Output() add: EventEmitter<boolean> = new EventEmitter();
+  title = '';
+  description = '';
 
+  addNewTask() {
+
+  }
 }
---
+++ src/app/models/task.ts
@@ -0,0 +1,12 @@
+export interface Task {
+    id?: string,
+    title:string, // 'title
+    description: string, // 'title
+    assintTo:string, // 'kevin', ' ahmet'
+    duDate:string, // '12.01.2023'
+    priority: string, 
+    catagory: string,
+    subTasks:string,
+}
+
+
