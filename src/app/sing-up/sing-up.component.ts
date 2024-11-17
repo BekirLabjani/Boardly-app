aa8c58328a3c226625488b2189d10f8f6f5475f5
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-11-17 19:40:06 +0100

    update signup

--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -3,7 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Auth } from '@angular/fire/auth';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
 import { GeneralFunktionsService } from '../service/general-funktions.service';
 import { User } from '../models/user-info';
 
@@ -12,40 +16,63 @@ import { User } from '../models/user-info';
   standalone: true,
   imports: [CommonModule, FormsModule], // CommonModule hier sicherstellen
   templateUrl: './sing-up.component.html',
-  styleUrl: './sing-up.component.scss'
+  styleUrl: './sing-up.component.scss',
 })
 export class SingUpComponent {
   user = new User();
   passwordMismatch = false;
   acceptPolicy = false;
+  showSuccessMessage = false;
 
-  constructor(private af: Auth, private generFunk: GeneralFunktionsService, private firestore: Firestore) {}
+  constructor(
+    private af: Auth,
+    private generFunk: GeneralFunktionsService,
+    private firestore: Firestore
+  ) {}
 
-  async regist(value: { name: string; email: string; password: string; confirmPassword: string }) {
-    if (value.password !== value.confirmPassword) {
-      this.passwordMismatch = true;
-      return;
-    } if(!this.acceptPolicy){
-      return;
-    }
+  async regist(value: {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }) {
+    this.stopRegistrationIfChechboxIsNotChecked();
     try {
-      const userCredential = await createUserWithEmailAndPassword(this.af, value.email, value.password);
+      const userCredential = await createUserWithEmailAndPassword(
+        this.af,
+        value.email,
+        value.password
+      );
       const user = userCredential.user;
       await updateProfile(user, { displayName: value.name });
       await addDoc(collection(this.firestore, 'users'), {
         userName: value.name,
         email: value.email,
       });
-      this.generFunk.openLogIn();
+      this.showSuccessMessage = true;
+      setTimeout(() => {
+        this.showSuccessMessage = false;
+        this.openLogin(); // Umleiten oder weitere Logik
+      }, 2000);
     } catch (error) {
       console.error('Error during registration:', error);
       alert('An error occurred during registration. Please try again.');
     }
   }
 
+  stopRegistrationIfChechboxIsNotChecked() {
+    if (!this.acceptPolicy) {
+      return;
+    }
+  }
+
   validatePasswords() {
-    const passwordInput = (document.getElementById('password') as HTMLInputElement)?.value;
-    const confirmPasswordInput = (document.getElementById('confirmPasswordInput') as HTMLInputElement)?.value;
+    const passwordInput = (
+      document.getElementById('password') as HTMLInputElement
+    )?.value;
+    const confirmPasswordInput = (
+      document.getElementById('confirmPasswordInput') as HTMLInputElement
+    )?.value;
     this.passwordMismatch = passwordInput !== confirmPasswordInput;
   }
 
@@ -53,4 +80,3 @@ export class SingUpComponent {
     this.generFunk.openLogIn();
   }
 }
-
