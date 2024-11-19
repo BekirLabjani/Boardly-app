d4d51aeb15f5970ee810e7ba8b7a4015ff05459d
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-11-19 19:58:22 +0100

    contacts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
+import { LetterAComponent } from './letters/letter-a/letter-a.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent, LetterAComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
---
+++ src/app/contact/letters/letter-a/letter-a.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './letter-a.component.html',
+  styleUrl: './letter-a.component.scss'
+})
+export class LetterAComponent {
+
+}
