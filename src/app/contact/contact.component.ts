317dfb72c6316ebab8f084cb8f59d93f1a3d6484
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-11-19 20:26:22 +0100

    transition

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
 import { LetterAComponent } from './letters/letter-a/letter-a.component';
+import { SidebarService } from '../service/side-bar-service.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,6 +11,36 @@ import { LetterAComponent } from './letters/letter-a/letter-a.component';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  isSidebarActive: boolean = true;
+
+  constructor(
+    private sidebarService: SidebarService,
+    private el: ElementRef
+  )
+    {}
+    ngOnInit(): void {
+      this.isSidebarActive = this.sidebarService.getSidebarStatus();
+      this.updateSidebarStyles();
+      
+      this.sidebarService.sidebarStatus$.subscribe(status => {
+        this.isSidebarActive = status;
+        this.updateSidebarStyles();
+      });
+    }
+  
+    private updateSidebarStyles() {
+      const contactMainElement = this.el.nativeElement.querySelector('.content');
+      if (this.isSidebarActive) {
+        contactMainElement.style.transform = 'translateX(0px)';
+        contactMainElement.style.transition = 'transform 400ms ease-in-out';
+      } else {
+        contactMainElement.style.transform = 'translateX(-200px)';
+      }
+    }
+  
+    toggleSidebar() {
+      this.sidebarService.toggleSidebar();
+    }
 }
