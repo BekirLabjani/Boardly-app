ac490c0007b0e2b216614157ef9fa440ed897ac7
Ahmet Yapicilar <a.yapicilar96@gmail.com>
2024-11-27 19:15:44 +0100

    fail

--- src/app/board/task-detail-dialog/task-detail-dialog.component.ts
+++ src/app/board/task-detail-dialog/task-detail-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input} from '@angular/core';
+import { AfterViewInit, Component, Inject, Input, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { Task } from '../../models/task';
 import { MatFormField } from '@angular/material/form-field';
@@ -27,7 +27,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // Importier
     ],
   styleUrls: ['./task-detail-dialog.component.scss'],
 })
-export class TaskDetailDialogComponent {
+export class TaskDetailDialogComponent implements AfterViewInit {
   tasks: Task[] = [];
   // Array zum Verwalten des Status der Subtasks (true/false)
   subTasksStatus: boolean[] = [];
@@ -39,16 +39,40 @@ export class TaskDetailDialogComponent {
     private dialogRef: MatDialogRef<TaskDetailDialogComponent>
   ) {
     // Initialisiere den Status basierend auf den vorhandenen Subtasks
-    this.subTasksStatus = Array(this.task.subTasks.length).fill(false);
+    // this.subTasksStatus = Array(this.task.subTasks.length).fill(false);
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
   }
 
   saveChanges(): void {
     // Beispiel: Subtasks-Status speichern oder verarbeiten
     console.log('Updated Subtasks Status:', this.subTasksStatus);
-    this.dialogRef.close({ updatedTask: this.task, subTasksStatus: this.subTasksStatus });
+    // this.dialogRef.close({ updatedTask: this.task, subTasksStatus: this.subTasksStatus });
+  }
+
+  subTaskCompleted(i: any){
+    this.subTasksStatus[i] = true;
+  }
+
+  setCheckedToInput(){
+    let content = document.getElementById('content');
+    do {
+      for (let i = 0; i < this.subTasksStatus.length; i++) {
+        const subTask = this.subTasksStatus[i];
+        let input = document.getElementById(`subTaskInput${i}`) as HTMLInputElement;
+        if(subTask == true){
+          input.checked = true;
+        }
+      }
+    } while (content);
   }
 
   openEditDialog(task: Task) {
+    //this.setCheckedToInput();
     const dialogRef = this.dialog.open(EditdialogComponent, { data: task });
   
     dialogRef.afterClosed().subscribe(result => {
