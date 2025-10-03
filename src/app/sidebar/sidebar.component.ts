cbd2f65fb0f4be6d4775939f61ef952cd5c71d21
BekirLabjani <business.life.bekir@gmail.com>
2025-10-03 21:00:52 +0200

    Update 0.1

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -14,49 +14,6 @@ import { DutyRosterComponent } from "../duty-roster/duty-roster.component";
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
-  isSidebarActive: boolean = true; // Standardmäßig aktiv
-
-  constructor(private router: Router, private sidebarService: SidebarService) {
-    // Initialisiere den Sidebar-Zustand beim Laden der Komponente
-    this.sidebarService.sidebarStatus$.subscribe(status => {
-      this.isSidebarActive = status;
-    });
-  }
-
-  // Toggle Sidebar aktivieren/deaktivieren
-  toggleSidebar() {
-    this.sidebarService.toggleSidebar();
-  }
-
-  navigateTo(event: Event, route: string) {
-    event.preventDefault();
-    this.router.navigateByUrl(route);
-  }
   
-
-  openPrivacyPolicy(event: Event) {
-    event.preventDefault();
-    this.router.navigateByUrl('/privacypolicy');
-  }
-
-  openLegalNotice(event: Event) {
-    event.preventDefault();
-    this.router.navigateByUrl('/legalnotice');
-  }
-
-  profileDropdownOpen = false;
-
-toggleProfileDropdown(event: Event) {
-  event.stopPropagation();
-  this.profileDropdownOpen = !this.profileDropdownOpen;
-}
-
-editProfile(event: Event) {
-  event.stopPropagation();
-  this.profileDropdownOpen = false;
-  // z. B. zur Profilseite navigieren:
-  this.router.navigate(['/edit-profile']);
-}
 
 }
